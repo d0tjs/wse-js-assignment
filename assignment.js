13152ac35c9d717ab7f5eafddd1fd40c7deed2d9
d0tjs <aappdotjs@gmail.com>
2023-10-24 17:55:34 +0600

    problem 6 solved

--- assignment.js
+++ assignment.js
@@ -51,3 +51,16 @@ function mergeArrays(array1, array2) {
 
   return mergedArray;
 }
+
+// Problem 6: Create a function isLeapYear that takes a year as a parameter and returns "This is a leap year" if it's a leap year, and "Not Leap year" otherwise.
+function isLeapYear(year) {
+  if (year % 400 == 0) {
+    return "This is a leap year";
+  } else if (year % 100 == 0) {
+    return "Not Leap year";
+  } else if (year % 4 == 0) {
+    return "This is a leap year";
+  } else {
+    return "Not Leap year";
+  }
+}
