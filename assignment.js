c275b509c2730215f4b97080400fa1acb1749ee7
d0tjs <aappdotjs@gmail.com>
2023-10-24 15:17:54 +0600

    problem 2 solved

--- assignment.js
+++ assignment.js
@@ -3,3 +3,10 @@ function triangleArea(base, height) {
   const area = (base * height) / 2;
   return area;
 }
+
+// Problem 2: Write a function to convert degrees to radians.
+function degreesToRadians(degree) {
+  let radians = degree * (Math.PI / 180);
+  radians = radians.toFixed(3); // toFixed is used to display 3 digits after decimal point
+  return radians;
+}
